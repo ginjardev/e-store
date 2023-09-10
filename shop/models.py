01da5385eb7a9d7eaebfa1541002c29d4b601ffb
ginjardev <arthurshur@gmail.com>
2023-09-10 21:41:03 +0000

    model product

--- shop/models.py
+++ shop/models.py
@@ -15,4 +15,28 @@ class Category(models.Model):
     def __str__(self):
         self.name
 
-    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to="products/%Y/%m/%d")
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-create'])
+        ]
+
+    def __str__(self):
+        self.name
+
+
+
