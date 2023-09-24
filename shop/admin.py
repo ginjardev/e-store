a5f8e250e2c6edd6a644270259c5368c313a971e
ginjardev <arthurshur@gmail.com>
2023-09-24 23:26:06 +0000

    product admin

--- shop/admin.py
+++ shop/admin.py
@@ -7,3 +7,11 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+@admin.register(Product)
+class ProdductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name', 'slug', 'price', 'available', 'created', 'updated'
+    ]
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
