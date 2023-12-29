3cd25f111b070a22b65c82ccdf3c57f9a1fb718e
ginjardev <arthurshur@gmail.com>
2023-12-29 10:30:30 +0000

    product list and detail templates

--- shop/views.py
+++ shop/views.py
@@ -19,7 +19,7 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
 	product = get_object_or_404(Product, id=id, slug=slug, available=True)
-	render(
+	return render(
 		request,
 		'shop/product/detail.html',
 		{'product': product}
